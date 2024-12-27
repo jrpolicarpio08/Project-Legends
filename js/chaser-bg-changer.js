0f80fbefb6b5ab5f31733f9b02b97e8b533d5534
jrpolicarpio08 <policarpiojohnroy@yahoo.com.ph>
2024-12-27 19:08:20 +0800

    chaser page java script and btn for build section

---
+++ js/chaser-bg-changer.js
@@ -0,0 +1,38 @@
+function carTrackPreview() {
+    const div = document.getElementById("chaser-track");
+
+    div.classList.add("fade-out");
+
+    setTimeout(() => {
+        div.style.backgroundImage = "url(../assets/img/chaser-track.png)";
+
+        div.classList.remove("fade-out");
+        div.classList.add("fade-in");
+    }, 200);
+}
+
+function engineTrackPreview() {
+    const div = document.getElementById("chaser-track");
+
+    div.classList.add("fade-out");
+
+    setTimeout(() => {
+        div.style.backgroundImage = "url(../assets/img/chaser-track-engine.png)";
+
+        div.classList.remove("fade-out");
+        div.classList.add("fade-in");
+    }, 200); 
+}
+
+function wheelTrackPreview() {
+    const div = document.getElementById("chaser-track");
+
+    div.classList.add("fade-out");
+
+    setTimeout(() => {
+        div.style.backgroundImage = "url(../assets/img/chaser-track-wheel.png)";
+
+        div.classList.remove("fade-out");
+        div.classList.add("fade-in");
+    }, 200); 
+}
---
+++ js/navlink-change-color-chaser.js
@@ -0,0 +1,47 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+const changeNavLinkColor = () => {
+    const scrollPosition = window.scrollY;
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            const sectionId = section.getAttribute('id');
+
+            let hoverColor;
+            if (sectionId === '1st') {
+                navLinks.forEach(link => link.style.color = 'black');
+                hoverColor = 'red';
+            } else if (sectionId === '2nd') {
+                navLinks.forEach(link => link.style.color = 'white');
+                hoverColor = 'blue';
+            } else if (sectionId === '3rd') {
+                navLinks.forEach(link => link.style.color = 'white');
+                hoverColor = 'green';
+            }
+
+            navLinks.forEach(link => {
+                link.addEventListener('mouseenter', () => {
+                    link.style.color = hoverColor;
+                });
+                link.addEventListener('mouseleave', () => {
+                    if (sectionId === '1st') {
+                        link.style.color = 'black';
+                    } else if (sectionId === '2nd') {
+                        link.style.color = 'white';
+                    } else if (sectionId === '3rd') {
+                        link.style.color = 'white';
+                    }
+                });
+            });
+        }
+    });
+};
+
+window.addEventListener('scroll', changeNavLinkColor);
+
+
