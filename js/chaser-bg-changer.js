8f2d32e7dbc86192016952186eddd8c0c96b4fad
jrpolicarpio08 <policarpiojohnroy@yahoo.com.ph>
2024-12-28 16:30:07 +0800

    remove some effets keep the change bg function

--- js/chaser-bg-changer.js
+++ js/chaser-bg-changer.js
@@ -38,33 +38,12 @@
 // }
 function carTrackPreview() {
     document.getElementById("chaser-track").style.backgroundImage = "url(../assets/img/chaser-track.png)";
-
-    div.classList.add("fade-out");
-
-    setTimeout(() => {
-         div.classList.remove("fade-out");
-         div.classList.add("fade-in");
-     }, 200); 
  }
 
  function engineTrackPreview() {
     document.getElementById("chaser-track").style.backgroundImage = "url(../assets/img/chaser-track-engine.png)";
-
-    div.classList.add("fade-out");
-
-    setTimeout(() => {
-        div.classList.remove("fade-out");
-        div.classList.add("fade-in");
-    }, 200); 
  }
 
 function wheelTrackPreview() {
     document.getElementById("chaser-track").style.backgroundImage = "url(../assets/img/chaser-track-wheel.png)";
-
-    div.classList.add("fade-out");
-
-    setTimeout(() => {
-        div.classList.remove("fade-out");
-        div.classList.add("fade-in");
-    }, 200); 
  }
