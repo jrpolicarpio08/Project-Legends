7375be1c779ce72767b1b02810d4ad37d76bdf38
jrpolicarpio08 <policarpiojohnroy@yahoo.com.ph>
2024-12-28 16:27:11 +0800

    js change for bg changer

--- js/chaser-bg-changer.js
+++ js/chaser-bg-changer.js
@@ -1,38 +1,70 @@
+// function carTrackPreview() {
+//     const div = document.getElementById("chaser-track");
+
+//     div.classList.add("fade-out");
+
+//     setTimeout(() => {
+//         div.style.backgroundImage = "url(../assets/img/chaser-track.png)";
+
+//         div.classList.remove("fade-out");
+//         div.classList.add("fade-in");
+//     }, 200);
+// }
+
+// function engineTrackPreview() {
+//     const div = document.getElementById("chaser-track");
+
+//     div.classList.add("fade-out");
+
+//     setTimeout(() => {
+//         div.style.backgroundImage = "url(../assets/img/chaser-track-engine.png)";
+
+//         div.classList.remove("fade-out");
+//         div.classList.add("fade-in");
+//     }, 200); 
+// }
+
+// function wheelTrackPreview() {
+//     const div = document.getElementById("chaser-track");
+
+//     div.classList.add("fade-out");
+
+//     setTimeout(() => {
+//         div.style.backgroundImage = "url(../assets/img/chaser-track-wheel.png)";
+
+//         div.classList.remove("fade-out");
+//         div.classList.add("fade-in");
+//     }, 200); 
+// }
 function carTrackPreview() {
-    const div = document.getElementById("chaser-track");
+    document.getElementById("chaser-track").style.backgroundImage = "url(../assets/img/chaser-track.png)";
 
     div.classList.add("fade-out");
 
     setTimeout(() => {
-        div.style.backgroundImage = "url(../assets/img/chaser-track.png)";
+         div.classList.remove("fade-out");
+         div.classList.add("fade-in");
+     }, 200); 
+ }
 
-        div.classList.remove("fade-out");
-        div.classList.add("fade-in");
-    }, 200);
-}
-
-function engineTrackPreview() {
-    const div = document.getElementById("chaser-track");
+ function engineTrackPreview() {
+    document.getElementById("chaser-track").style.backgroundImage = "url(../assets/img/chaser-track-engine.png)";
 
     div.classList.add("fade-out");
 
     setTimeout(() => {
-        div.style.backgroundImage = "url(../assets/img/chaser-track-engine.png)";
-
         div.classList.remove("fade-out");
         div.classList.add("fade-in");
     }, 200); 
-}
+ }
 
 function wheelTrackPreview() {
-    const div = document.getElementById("chaser-track");
+    document.getElementById("chaser-track").style.backgroundImage = "url(../assets/img/chaser-track-wheel.png)";
 
     div.classList.add("fade-out");
 
     setTimeout(() => {
-        div.style.backgroundImage = "url(../assets/img/chaser-track-wheel.png)";
-
         div.classList.remove("fade-out");
         div.classList.add("fade-in");
     }, 200); 
-}
+ }
