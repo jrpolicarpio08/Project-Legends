fb95557aaefa1ba265c180ad5f014e8fae5f340d
jrpolicarpio08 <policarpiojohnroy@yahoo.com.ph>
2025-01-09 17:02:10 +0800

    chaser page images added and update function JS for build variant

--- js/chaser-bg-changer.js
+++ js/chaser-bg-changer.js
@@ -1,3 +1,18 @@
+function carDriftPreview() {
+   const newBackground = "url('/Project-Legends/assets/img/chaser-drift.png')";
+   document.getElementById("chaser-track").style.backgroundImage = newBackground;
+}
+
+function engineDriftPreview() {
+   const newBackground = "url('/Project-Legends/assets/img/chaser-drift-engine.png')";
+   document.getElementById("chaser-track").style.backgroundImage = newBackground;
+}
+
+function wheelDriftPreview() {
+   const newBackground = "url('/Project-Legends/assets/img/chaser-drift-wheel.png')";
+   document.getElementById("chaser-track").style.backgroundImage = newBackground;
+}
+ 
  function carTrackPreview() {
    const newBackground = "url('/Project-Legends/assets/img/chaser-track.png')";
    document.getElementById("chaser-track").style.backgroundImage = newBackground;
@@ -12,3 +27,19 @@ function wheelTrackPreview() {
    const newBackground = "url('/Project-Legends/assets/img/chaser-track-wheel.png')";
    document.getElementById("chaser-track").style.backgroundImage = newBackground;
 }
+
+function carOemPreview() {
+   const newBackground = "url('/Project-Legends/assets/img/chaser-oem.png')";
+   document.getElementById("chaser-track").style.backgroundImage = newBackground;
+}
+
+function engineOemPreview() {
+   const newBackground = "url('/Project-Legends/assets/img/chaser-oem-engine.png')";
+   document.getElementById("chaser-track").style.backgroundImage = newBackground;
+}
+
+function wheelOemPreview() {
+   const newBackground = "url('/Project-Legends/assets/img/chaser-oem-wheel.png')";
+   document.getElementById("chaser-track").style.backgroundImage = newBackground;
+}
+
